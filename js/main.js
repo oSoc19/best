d026f2b692fa719283911c0003a2eb670bcedce8
brechtvdv <spinnewebber_toon@hotmail.com>
2015-03-31 19:42:01 +0200

    Added Student-section and whole bunch of other tweaks

--- js/main.js
+++ js/main.js
@@ -33,4 +33,16 @@ $(function(){
         ga('send', 'click', 'contact', 'press');
     });
 
+    /* FAQ */
+    
+    $(document).ready(function() {
+    $('#faqs h3').each(function() {
+        var tis = $(this), state = false, answer = tis.next('div').hide().css('height','auto').slideUp();
+        tis.click(function() {
+            state = !state;
+            answer.slideToggle(state);
+            tis.toggleClass('active',state);
+        });
+    });
+});
 });
