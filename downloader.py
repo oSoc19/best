b736476885224c53973a0959faffca652a1f1094
JosseVanDelm <josse.vandelm@gmail.com>
2019-07-03 11:27:57 +0200

    Working version of unzipper. Further testing needed.

--- downloader.py
+++ downloader.py
@@ -1,7 +1,9 @@
-import logging          # for logging purposes
-import zipfile          # for zipping/unzipping files
-import os               # for os related stuff, like walking through direcory structures
-from pathlib import Path     # object-oriented paths
+import logging                  # for logging purposes
+import zipfile                  # for zipping/unzipping files
+import os                       # for os related stuff, like walking through direcory structures
+from pathlib  import Path       # object-oriented paths
+import argparse                 # for command-line argument parsing
+import requests
 
 # Setup logger - (Python logger breaks PEP8 by default)
 logger = logging.getLogger(__name__)
@@ -16,27 +18,40 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-def unzip_recursive(zipped_file,to_folder,set_remove=True):
+def unzip_recursive(zipped_file, to_folder, set_remove=True):
     """
-    Function that recursively goes through folders and unpacks zip them.
+    Function that recursively goes through folders and unpacks zips inside them.
+    All unzipped files are stored in the same folder (output_folder)
     """
     print(to_folder)
     with zipfile.ZipFile(zipped_file, 'r') as zfile:
         #TODO catch exceptions
-        zfile.extractall(path=Path(to_folder))
-    # remove the zip file
+        zfile.extractall(path=to_folder)
+    # if set_remove is True, remove the original zip file after extraction
     if (set_remove):
         os.remove(zipped_file)
     # walk through the selected folder
     for dir_name, subdir_list, file_list in os.walk(to_folder):
-        absolute_path = Path(to_folder).parent / Path(dir_name)
-        logger.info('inside folder: {}'.format(absolute_path))
+        logger.info('inside folder: {}'.format(to_folder))
         for specific_file in file_list:
+            print(specific_file)
+            specific_file_path = Path(specific_file)
             # look for zip-files
-            if (Path(specific_file).suffix == '.zip'):
+            if (specific_file_path.suffix =='.zip'):
+                new_file_path = (Path(dir_name) / specific_file_path)
                 # if it is a zip file, extract its contents and enter the folder, then unzip and look for files again.
-                unzip_recursive(specific_file,absolute_path)
+                unzip_recursive(new_file_path,to_folder)
+
+logger.info("Start download")
+
+logger.info("Download done")
 
 logger.info("Start extraction")
-unzip_recursive("/home/osoc19/best/foldertje.zip","/home/osoc19/best/foldertje",False)
+# create the output directory
+output_directory = Path("/home/osoc19/best/unzip_output")
+try:
+    os.mkdir(output_directory)
+except FileExistsError:
+    logger.error("Output directory already exists")
+unzip_recursive(Path("/home/osoc19/best/foldertje.zip"),Path("/home/osoc19/best/foldertje"),False)
 logger.info("Done")
