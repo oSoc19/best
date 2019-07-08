fd5475954674c76eaaf784ec9122a23998aa7e81
Theo Dedeken <theo.dedeken@telenet.be>
2019-07-08 16:50:49 +0200

    Filter on bounding box

--- filter/filter.py
+++ filter/filter.py
@@ -16,8 +16,18 @@ def filter_file(args):
     result = file
     # select the addresses for a certain postcode
     if args.postcode:
-        result = file[file['postcode'] == args.postcode]
+        result = result[result['postcode'] == args.postcode]
 
+    # Select the addresses in a bounding box
+    if args.bbox:
+        result = result[
+            (args.bbox[0] <= result['EPSG:4326_x']) &
+            (result['EPSG:4326_x'] <= args.bbox[1]) &
+            (args.bbox[2] <= result['EPSG:4326_y']) &
+            (result['EPSG:4326_y'] <= args.bbox[3])
+        ]
+
+    # if we only need streetnames, drop the unnecessary attributes
     if args.out_fmt == 'street':
         result = result.drop(
             ['EPSG:31370_x',
@@ -43,10 +53,11 @@ if __name__ == "__main__":
     parser.add_argument(
         'input_file', help='input address file')
     parser.add_argument('output_file', help='output file')
-    parser.add_argument('--out_fmt', default='address', choices=[
+    parser.add_argument('--output_type', default='address', choices=[
                         'address', 'street'], help='Contents of the output, either full addresses or streetnames')
     parser.add_argument('--postcode', type=int, help='postcode to filter on')
-    parser.add_argument('--bbox', help='bounding box')
+    parser.add_argument(
+        '--bbox', type=float, nargs=4, help='Bounding box to filter on, format: min_x max_x min_y max_y (in EPSG:4326 coordinates)')
 
     args = parser.parse_args()
     filter_file(args)
